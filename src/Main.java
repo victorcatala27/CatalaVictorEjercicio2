e47c3c1634e9c1d97719257e6986fc0c1afce98e
Victorcatala27 <vcabo00@gmail.com>
2024-11-16 17:36:22 +0100

    Loop condition was added to avoid the user from entering invalid data. It won't stop until a valid number is given

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input= new Scanner(System.in);
-        char patron=
+
+        System.out.println("Introduce un valor o símbolo");
+        char patron= input.next().charAt(0);            //Sentencia para leer únicamente el primer caracter introducido
+
+        int tamany=0;
+
+        while (true){                                   //Bucle para asegurar que el usuario introduce los datos solicitados
+            System.out.println("Introduce un tamaño para el tablero (de 1 a 15)");
+            if(input.hasNextInt()){
+                tamany=input.nextInt();
+                if(tamany>=1 && tamany<=15){
+                    break;
+                }
+            }
+          else{
+                input.next();
+            }
+            System.out.println("Valor erróneo. Por favor, introduce un valor comprendido entre 1 y 15 ambos incluidos");
+        }
+
+
     }
 }
---
+++ src/README.md
@@ -0,0 +1,3 @@
+Este es el segundo ejercicio de la asignatura Programación estructurada, donde a partir de introducir un símbolo o caracter por pantalla y un tamaño comprendido entre los valores 1 y 15 ambos incluidos, se generará un tablero de ajedrez personalizado.
+
+¡Espero que os guste!
