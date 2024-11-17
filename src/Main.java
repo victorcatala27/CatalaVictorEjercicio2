47f2baddad3798db419a2587a3dfc0e00788866f
Victorcatala27 <vcabo00@gmail.com>
2024-11-17 22:05:43 +0100

    Adding code to create the chessboard and the description

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,11 @@ public class Main {
         Scanner input= new Scanner(System.in);
 
         System.out.println("Introduce un valor o símbolo");
-        char patron= input.next().charAt(0);            //Sentencia para leer únicamente el primer caracter introducido
+        char patron= input.next().charAt(0);                        //Sentencia para leer únicamente el primer caracter introducido
 
         int tamany=0;
 
-        while (true){                                   //Bucle para asegurar que el usuario introduce los datos solicitados
+        while (true){                                               //Bucle para asegurar que el usuario introduce los datos solicitados
             System.out.println("Introduce un tamaño para el tablero (de 1 a 15)");
             if(input.hasNextInt()){
                 tamany=input.nextInt();
@@ -22,7 +22,23 @@ public class Main {
             }
             System.out.println("Valor erróneo. Por favor, introduce un valor comprendido entre 1 y 15 ambos incluidos");
         }
-
+    for (int fila=0; fila<8;fila++){                                                  //Itera sobre las 8 filas del tablero
+        for (int i=0; i<tamany; i++){                                                //Altura de la fila
+            for (int columna=0; columna<8;columna++){
+                char imprimirTablero;
+                if ((fila+columna)%2==0){
+                    imprimirTablero=patron;
+                }else {
+                    imprimirTablero=' ';
+                }
+                //Controla la anchura
+                for (int j=0; j<tamany;j++) {
+                    System.out.print(imprimirTablero);
+                }
+            }
+            System.out.println();
+        }
+    }
 
     }
 }
--- src/README.md
+++ src/README.md
@@ -1,3 +1,4 @@
 Este es el segundo ejercicio de la asignatura Programación estructurada, donde a partir de introducir un símbolo o caracter por pantalla y un tamaño comprendido entre los valores 1 y 15 ambos incluidos, se generará un tablero de ajedrez personalizado.
+El programa pedirá en primer lugar un valor a elegir, seguido de un tamaño y creará el tablero definido por el usuario
 
 ¡Espero que os guste!
